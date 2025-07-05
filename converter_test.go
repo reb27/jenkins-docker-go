9ad00a7f36e9db84a65f29908f04c6a6943e1826
Rebeca Pedrosa <46985866+reb27@users.noreply.github.com>
2025-07-04 23:29:12 -0300

    Update converter_test.go

--- converter_test.go
+++ converter_test.go
@@ -4,7 +4,7 @@ import "testing"
 
 func TestKilometersToMiles(t *testing.T) {
     result := KilometersToMiles(1.0)
-    expected := 0.621371
+    expected := 0.6213
     if result != expected {
         t.Errorf("Expected %.6f, got %.6f", expected, result)
     }
@@ -12,7 +12,7 @@ func TestKilometersToMiles(t *testing.T) {
 
 func TestMilesToKilometers(t *testing.T) {
     result := MilesToKilometers(0.621371)
-    expected := 1.0
+    expected := 0.0
     if result != expected {
         t.Errorf("Expected %.6f, got %.6f", expected, result)
     }
